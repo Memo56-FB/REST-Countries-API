b44f8e84b6f369c65ebf0ae109dae5f09af55ffc
Guillermo Farfan <memo56_fb@hotmail.com>
2021-07-17 13:14:42 -0500

    Responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import {light, dark} from './theme/theme'
 import {GlobalStyle} from './theme/global'
@@ -23,11 +23,12 @@ export default function App() {
               <h2>Where in the world</h2>
               <button onClick={()=>{themeToggler()}} ><ion-icon name="moon"></ion-icon> </button>    
             </header>
+            {/* <Redirect exact from="/" to="/countries" /> */}
             <Switch>
-              <Route path="/countries">
+              <Route exact path="/countries">
                 <Countries/>
               </Route>
-              <Route path="/about">
+              <Route exact path="/about">
                 <AboutCountry/>
               </Route>
             </Switch>
